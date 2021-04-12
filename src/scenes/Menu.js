297515438594e89a4c33b94cb14c31bc5d47be74
Max <mrgasser@ucsc.edu>
2021-04-12 11:59:01 -0700

    Added dificulty functionality

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -35,6 +35,28 @@ class Menu extends Phaser.Scene {
 
         //define keys
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            // easy mode
+            game.settings = {
+              spaceshipSpeed: 3,
+              gameTimer: 60000    
+            }
+            this.sound.play('sfx_select');
+            this.scene.start('playScene');    
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+            // hard mode
+            game.settings = {
+              spaceshipSpeed: 4,
+              gameTimer: 45000    
+            }
+            this.sound.play('sfx_select');
+            this.scene.start('playScene');    
+        }
     }
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -92,7 +92,7 @@ class Play extends Phaser.Scene {
         this.gameOver = false;
         // 60 second play clock
         scoreConfig.fixedWidth = 0;
-        this.clock = this.time.delayedCall(60000, () => {
+        this.clock = this.time.delayedCall(10000, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER',
             scoreConfig).setOrigin(0.5);
             this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart',
@@ -162,4 +162,4 @@ class Play extends Phaser.Scene {
         this.scoreLeft.text = this.p1Score;
         this.sound.play('sfx_explosion');
     }
-}r
+}
